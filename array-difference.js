09b6cda517d8a3a4257e6673fd9b92a409c1de01
Patrick Gross <bears_lair@protonmail.com>
2024-04-17 18:17:26 -0700

    final commit

--- array-difference.js
+++ array-difference.js
@@ -1,8 +1,16 @@
-function arrayDiff(a, b) {}
+// if a or b is an empty array, return a
+// loop through a, adding to new array empty array elements that are not in b
 
-console.log(arrayDiff([1, 2], [1])); // [2]
-console.log(arrayDiff([1, 2, 2], [1])); // [2, 2]
-console.log(arrayDiff([1, 2, 2], [2])); //[1]
-console.log(arrayDiff([1, 2, 2], [])); // [1, 2, 2]
-console.log(arrayDiff([], [1, 2])); // []
-console.log(arrayDiff([1, 2, 3], [1, 2])); // [3]
+function arrayDiff(a, b) {
+  if (b.length === 0) {
+    return a;
+  } else if (a.length === 0) {
+    return [];
+  } else {
+    let difference = [];
+    a.forEach((element) =>
+      !b.includes(element) ? difference.push(element) : null
+    );
+    return difference;
+  }
+}
